0dd57f01dbecf52782b031f4394b8352bdcda911
wenchijchang <heyyhey@hotmail.com>
2023-03-06 18:26:32 -0500

    added note for bootstrap input group in SearchBar

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -16,3 +16,11 @@ const SearchBar = ({ userInput, setUserInput }) => {
 };
 
 export default SearchBar;
+
+// bootstrap input group
+// <div class="input-group mb-3">
+//   <div class="input-group-prepend">
+//     <span class="input-group-text" id="basic-addon1">Search</span>
+//   </div>
+//   <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+// </div>
